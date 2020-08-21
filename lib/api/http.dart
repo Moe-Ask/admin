616c27a2d12585fcf4bde00511d192a97984c78f
orange soeur <juzi201314@gmail.com>
2020-08-21 15:06:46 +0800

    将api.moeask.com更改为api.moeask.net

--- lib/api/http.dart
+++ lib/api/http.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-const MainApi = "https://api.moeask.com/v1";
+const MainApi = "https://api.moeask.net/v1";
 
 http.Client _client;
 
